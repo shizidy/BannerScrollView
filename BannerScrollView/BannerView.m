a3da242ec4e8a53abbe711f641124df36e9e47f3
wangdongya <wangdongya1990@163.com>
2022-06-10 13:21:52 +0800

    update

--- BannerScrollView/BannerView.m
+++ BannerScrollView/BannerView.m
@@ -22,7 +22,7 @@
 @end
 
 @implementation BannerView
--(instancetype)initWithFrame:(CGRect)frame imageArray:(NSArray *)imageArr{
+-(instancetype)initWithFrame:(CGRect)frame imageArray:(NSArray *)imageArr {
     if (self = [super initWithFrame:frame]) {
         self.imageArr = [NSMutableArray arrayWithArray:imageArr];
         [self.imageArr insertObject:self.imageArr[self.imageArr.count-1] atIndex:0];
@@ -38,15 +38,15 @@
     return self;
 }
 #pragma mark ========== UIScrollViewDelegate ==========
--(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
     CGFloat offsetX = scrollView.contentOffset.x/scrollView.frame.size.width;
     NSLog(@"%.1f", offsetX);
     //调整数据源数据位置
-    if (offsetX==0) {
+    if (offsetX == 0) {
         [self.imageArr insertObject:self.imageArr[self.imageArr.count-1] atIndex:0];
         [self.imageArr removeObjectAtIndex:self.imageArr.count-1];
     }
-    if (offsetX==2) {
+    if (offsetX == 2) {
         [self.imageArr addObject:self.imageArr[0]];
         [self.imageArr removeObjectAtIndex:0];
     }
@@ -56,30 +56,30 @@
     [self.scrollView setContentOffset:CGPointMake(self.frame.size.width, 0) animated:NO];
 }
 #pragma mark ========== 懒加载 ==========
--(UIScrollView *)scrollView{
-    if (_scrollView==nil) {
+- (UIScrollView *)scrollView {
+    if (_scrollView == nil) {
         _scrollView = [[UIScrollView alloc] initWithFrame:self.bounds];
         _scrollView.delegate = self;
         _scrollView.contentSize = CGSizeMake(self.frame.size.width*3, 200);
         _scrollView.contentOffset = CGPointMake(self.frame.size.width, 0);
         _scrollView.pagingEnabled = YES;
         _scrollView.backgroundColor = [UIColor whiteColor];
-        for (int i=0; i<3; i++) {
+        for (int i = 0; i < 3; i++) {
 //            UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(i*self.frame.size.width, 0, self.frame.size.width, self.frame.size.height)];
 //            [_scrollView addSubview:imgView];
-            UILabel *lb = [[UILabel alloc] initWithFrame:CGRectMake(i*self.frame.size.width, 0, self.frame.size.width, self.frame.size.height)];
+            UILabel *lb = [[UILabel alloc] initWithFrame:CGRectMake(i * self.frame.size.width, 0, self.frame.size.width, self.frame.size.height)];
             [_scrollView addSubview:lb];
             lb.text = self.imageArr[i];
             lb.textAlignment = NSTextAlignmentCenter;
             lb.textColor = [UIColor whiteColor];
             lb.backgroundColor = [UIColor orangeColor];
-            if (i==0) {
+            if (i == 0) {
                 self.leftlb = lb;
             }
-            if (i==1) {
+            if (i == 1) {
                 self.centerlb = lb;
             }
-            if (i==2) {
+            if (i == 2) {
                 self.rightlb = lb;
             }
         }
